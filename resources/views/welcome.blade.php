5e2898742d608fee25e0715ad674a376b52315b9
Anish <anishthapa9039@gmail.com>
2025-02-14 17:15:02 +0545

    Added all

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body  class="bg-gray-800" >
+    <h1 class="text-3xl text-white  " >CRUD operation</h1>
+</body>
+</html>
