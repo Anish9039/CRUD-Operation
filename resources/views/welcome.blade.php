43aeb96f116f7373834bc0b0b43b103bc0464df2
Anish <anishthapa9039@gmail.com>
2025-02-14 18:05:49 +0545

    connected to mysql database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
     <title>Document</title>
     @vite(['resources/css/app.css'])
 </head>
-<body  class="bg-gray-800" >
-    <h1 class="text-3xl text-white  " >CRUD operation</h1>
+<body  class="bg-gray-800">
+    <h1 class="text-3xl text-white  text-center">CRUD operation</h1>
+
+    <button class="text-white text-center" >Create</button>
 </body>
 </html>
